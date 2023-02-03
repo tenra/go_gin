4bc917a5d3682116e02b56b3d2c31e62e4e6105d
tenra <tetenchi@yahoo.co.jp>
2023-02-03 10:34:05 +0900

    auth0

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
     "os"
     "time"
+    //"log"
+	"net/http"
 
     "github.com/gin-gonic/gin"
     "github.com/tenra/go_gin/promotion"
@@ -12,6 +14,8 @@ import (
     "github.com/joho/godotenv"
 
     "github.com/gin-contrib/cors"
+
+    "github.com/tenra/go_gin/middleware"
 )
 
 func main() {
@@ -50,6 +54,29 @@ func main() {
         AllowCredentials: true,
         MaxAge:           24 * time.Hour,
     }))
+
+    router := http.NewServeMux()
+
+	// This route is always accessible.
+	router.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message":"Hello from a public endpoint! You don't need to be authenticated to see this."}`))
+	}))
+
+	// This route is only accessible if the user has a valid access_token.
+	router.Handle("/mypage", middleware.EnsureValidToken()(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// CORS Headers.
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Origin", os.Getenv("FRONT_URL"))
+			w.Header().Set("Access-Control-Allow-Headers", "Authorization")
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`{"message":"Hello from a private endpoint! You need to be authenticated to see this."}`))
+		}),
+	))
 
     r.GET("/promotions", handler.Index(promotion))
     r.POST("/promotions", handler.Create(promotion))
